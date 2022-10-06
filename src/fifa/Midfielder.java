04b8a669f0c57f33475a288baaee3fc61658a042
18d_kecskesk <kecskeskrisztiaan@gmail.com>
2022-10-06 13:53:14 +0200

    asd

--- src/fifa/Midfielder.java
+++ src/fifa/Midfielder.java
@@ -3,6 +3,20 @@ package fifa;
 public class Midfielder {
     private String name;
     private MidPositions position;
+    private Paramet paramets;
+    private String national;
+    private String club;
+    private boolean weakFoot;
+    private int skillMoves;
 
 
+    public Midfielder(String name,  MidPositions position, Paramet paramets, String national, String club, boolean weakFoot, int skillMoves) {
+        this.name = name;
+        this.position = position;
+        this.paramets = paramets;
+        this.national = national;
+        this.club = club;
+        this.weakFoot = weakFoot;
+        this.skillMoves = skillMoves;
+    }
 }
---
+++ src/fifa/Paramet.java
@@ -0,0 +1,10 @@
+package fifa;
+
+public enum Paramet {
+    PAC,
+    DRI,
+    SHO,
+    DEF,
+    PAS,
+    PHY;
+}
