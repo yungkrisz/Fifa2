bf261bc03a060cd1f48cfc5dbc25521dbacda1ac
18d_kecskesk <kecskeskrisztiaan@gmail.com>
2022-10-07 09:12:25 +0200

    Midfielder done

--- src/fifa/Midfielder.java
+++ src/fifa/Midfielder.java
@@ -2,6 +2,7 @@ package fifa;
 
 public class Midfielder {
     private String name;
+    private int rating;
     private MidPositions position;
     private String nation;
     private String club;
@@ -9,8 +10,9 @@ public class Midfielder {
     private int skillMoves;
 
 
-    public Midfielder(String name,  MidPositions position, String nation, String club, boolean weakFoot, int skillMoves) {
+    public Midfielder(String name, int rating, MidPositions position, String nation, String club, boolean weakFoot, int skillMoves) {
         this.name = name;
+        this.rating = rating;
         this.position = position;
         this.nation = nation;
         this.club = club;
@@ -18,8 +20,36 @@ public class Midfielder {
         this.skillMoves = skillMoves;
     }
 
+    public String getName() {
+        return name;
+    }
+
+    public int getRating() {
+        return rating;
+    }
+
+    public MidPositions getPosition() {
+        return position;
+    }
+
+    public String getNation() {
+        return nation;
+    }
+
+    public String getClub() {
+        return club;
+    }
+
+    public boolean isWeakFoot() {
+        return weakFoot;
+    }
+
+    public int getSkillMoves() {
+        return skillMoves;
+    }
+
     @Override
     public String toString() {
-        return "Középpályás : " + name + position + nation + club + weakFoot + skillMoves;
+        return "Középpályás : " + name + rating + position + nation + club + weakFoot + skillMoves;
     }
 }
