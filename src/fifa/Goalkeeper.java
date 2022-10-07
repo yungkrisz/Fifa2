f955960a8ed614ebe7f3d8d49dd5c96b2169351b
Lukacs Donat <lukacsdonatxxx@gmail.com>
2022-10-07 08:51:59 +0200

    goalkeeper done

--- src/fifa/Goalkeeper.java
+++ src/fifa/Goalkeeper.java
@@ -1,6 +1,44 @@
 package fifa;
 
 public class Goalkeeper {
-    int rating;
-    String name;
+    private int rating;
+    private String name;
+    private String national;
+    private String club;
+    private boolean weakFoot;
+    private int skillMoves;
+
+    public Goalkeeper(int rating, String name, String national, String club, boolean weakFoot, int skillMoves) {
+        this.rating = rating;
+        this.name = name;
+        this.national = national;
+        this.club = club;
+        this.weakFoot = weakFoot;
+        this.skillMoves = skillMoves;
+    }
+
+    public int getRating() {
+        return rating;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getNational() {
+        return national;
+    }
+
+    public String getClub() {
+        return club;
+    }
+
+    public boolean isWeakFoot() {
+        return weakFoot;
+    }
+
+    public int getSkillMoves() {
+        return skillMoves;
+    }
+
 }
