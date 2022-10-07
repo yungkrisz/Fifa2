c78e638ed96b0bb500f9206cc24b3028d8475d8d
18d_kecskesk <kecskeskrisztiaan@gmail.com>
2022-10-07 09:03:08 +0200

    Attacker done

--- src/fifa/Attacker.java
+++ src/fifa/Attacker.java
@@ -2,14 +2,16 @@ package fifa;
 
 public class Attacker {
     private String name;
+    private int rating;
     private AttPositions position;
     private String nation;
     private String club;
     private boolean weakFoot;
     private int skillMoves;
 
-    public Attacker(String name, AttPositions position, String nation, String club, boolean weakFoot, int skillMoves) {
+    public Attacker(String name, int rating, AttPositions position, String nation, String club, boolean weakFoot, int skillMoves) {
         this.name = name;
+        this.rating = rating;
         this.position = position;
         this.nation = nation;
         this.club = club;
@@ -17,8 +19,34 @@ public class Attacker {
         this.skillMoves = skillMoves;
     }
 
+    public String getName() {
+        return name;
+    }
+    public int getRating(){
+        return rating;
+    }
+    public AttPositions getPosition() {
+        return position;
+    }
+
+    public String getNation() {
+        return nation;
+    }
+
+    public String getClub() {
+        return club;
+    }
+
+    public boolean isWeakFoot() {
+        return weakFoot;
+    }
+
+    public int getSkillMoves() {
+        return skillMoves;
+    }
+
     @Override
     public String toString() {
-        return "Támadó : " + name + position + nation + club + weakFoot + skillMoves;
+        return "Támadó : " + name + rating + position + nation + club + weakFoot + skillMoves;
     }
 }
