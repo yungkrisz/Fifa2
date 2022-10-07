2fa748d1497a0259dc4ad34c1b78f4f3bcdbc59c
18d_kecskesk <kecskeskrisztiaan@gmail.com>
2022-10-07 08:54:32 +0200

    Defender, Attacker toString method creat

--- src/fifa/Attacker.java
+++ src/fifa/Attacker.java
@@ -3,17 +3,22 @@ package fifa;
 public class Attacker {
     private String name;
     private AttPositions position;
-    private String national;
+    private String nation;
     private String club;
     private boolean weakFoot;
     private int skillMoves;
 
-    public Attacker(String name, AttPositions position, String national, String club, boolean weakFoot, int skillMoves) {
+    public Attacker(String name, AttPositions position, String nation, String club, boolean weakFoot, int skillMoves) {
         this.name = name;
         this.position = position;
-        this.national = national;
+        this.nation = nation;
         this.club = club;
         this.weakFoot = weakFoot;
         this.skillMoves = skillMoves;
+    }
+
+    @Override
+    public String toString() {
+        return "Támadó : " + name + position + nation + club + weakFoot + skillMoves;
     }
 }
--- src/fifa/Defender.java
+++ src/fifa/Defender.java
@@ -3,17 +3,22 @@ package fifa;
 public class Defender {
     private String name;
     private DefPositions position;
-    private String national;
+    private String nation;
     private String club;
     private boolean weakFoot;
     private int skillMoves;
 
-    public Defender(String name, DefPositions position, String national, String club, boolean weakFoot, int skillMoves) {
+    public Defender(String name, DefPositions position, String nation, String club, boolean weakFoot, int skillMoves) {
         this.name = name;
         this.position = position;
-        this.national = national;
+        this.nation = nation;
         this.club = club;
         this.weakFoot = weakFoot;
         this.skillMoves = skillMoves;
+    }
+
+    @Override
+    public String toString() {
+        return "Védekező : " + name + position + nation + club + weakFoot + skillMoves;
     }
 }
