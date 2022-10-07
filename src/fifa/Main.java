f8c745309d828d0e2f61bbbd1a9a9988aabb8b60
18d_kecskesk <kecskeskrisztiaan@gmail.com>
2022-10-07 09:12:57 +0200

    main done

--- src/fifa/Main.java
+++ src/fifa/Main.java
@@ -2,7 +2,16 @@ package fifa;
 
 public class Main {
     public static void main(String[] args) {
-    Midfielder Modric = new Midfielder("Modric",MidPositions.CM,"Croatia","Real Madrid",false,5);
+        Goalkeeper Yashin = new Goalkeeper(95,"Yashin", "Russia", "Icon", false, 3  );
+        System.out.println(Yashin);
+
+        Defender Ramos = new Defender("Ramos",88,DefPositions.CB,"Spain","PSG",false,3);
+        System.out.println(Ramos);
+
+        Midfielder Modric = new Midfielder("Modric", 89, MidPositions.CM,"Croatia","Real Madrid",false,5);
         System.out.println(Modric);
+
+        Attacker Ronaldo = new Attacker("Ronaldo", 90, AttPositions.ST,"Portugal","ManUtd",true,5);
+        System.out.println(Ronaldo);
     }
 }
