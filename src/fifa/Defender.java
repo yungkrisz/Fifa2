4a87d7a1c76aa0c5af5ec867f4c395bb8870399b
18d_kecskesk <kecskeskrisztiaan@gmail.com>
2022-10-07 09:15:24 +0200

    Defender done

--- src/fifa/Defender.java
+++ src/fifa/Defender.java
@@ -47,6 +47,6 @@ public class Defender {
 
     @Override
     public String toString() {
-        return "Védekező : " + name + rating + position + nation + club + weakFoot + skillMoves;
+        return "Védekező : " + name + " " + rating + " " + position + " " + nation + " " + club + " " + weakFoot + " " + skillMoves;
     }
 }
